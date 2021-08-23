b3233a7da26bd1124eb85abe7e270bebee276a82
mati9012 <dlamati9012@gmail.com>
2021-08-23 15:32:42 +0200

    Update

--- src/main/java/main/main/Help.java
+++ src/main/java/main/main/Help.java
@@ -7,17 +7,12 @@ public class Help {
     }
 
     public static void sendHelp(Player p) {
-        p.sendMessage("§c--- HypixelTNT Help ---");
+        p.sendMessage("§c--- BestTnT Help ---");
         p.sendMessage("");
         p.sendMessage("§cThis Plugin adds the TNT and Fireballs of Hypixel Bedwars");
         p.sendMessage("");
-        p.sendMessage("§cTNT will be automaticly ignited when placed");
-        p.sendMessage("§cRightclick while holding a fireball to shoot it");
         p.sendMessage("");
         p.sendMessage("§cCommands:");
-        p.sendMessage("§c - /tnt (Adds an INFINITE TnT to your Inventory");
-        p.sendMessage("§c - /fireball (Adds an INFINITE Fireball to your Inventory");
-        p.sendMessage("§c - /hypixeltnt (Shows this help)");
-        p.sendMessage("§c - Use /togglehelp to disable this help");
+        p.sendMessage("§cNull");
     }
 }
